d4224724b2939746f6a01f585d47da0f160951ce
Nikko Lumahang <kertnik@gmail.com>
2020-12-04 17:32:19 +0800

    added insterests & projects pages

--- pages/interests.vue
+++ pages/interests.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    This is the interests page
-  </div>
+  <section class="resume-section">
+    <div class="resume-section-content">
+      <h2 class="mb-5">
+        {{ title }}
+      </h2>
+      <p>
+        Apart from being a web developer, I enjoy most of my time being outdoors.
+        During the warmer months, I enjoy mountain climbing, and cycling.
+      </p>
+      <p class="mb-0">
+        When forced indoors, I follow a number of sci-fi and fantasy genre movies and
+        television shows, I am an aspiring musician, and I spend a large amount of my free
+        time exploring the latest technology advancements in the front-end web development
+        world.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>
-    This is the projects page
-  </div>
+  <section class="resume-section">
+    <div class="resume-section-content">
+      <h2 class="mb-5">
+        {{ title }}
+      </h2>
+      <div class="row">
+        <div v-for="project in projects" :key="project.id" class="col-sm">
+          <div class="card mb-3" style="width: 19rem;">
+            <div class="card-header bg-light">
+              <h3 class="card-title text-center text-dark">
+                <a :href="project.github_link" target="_blank">
+                  {{ project.title }}
+                </a>
+              </h3>
+            </div>
+            <div class="card-body">
+              {{ project.description }}
+            </div>
+            <div class="card-footer">
+              <span v-for="tool in project.tools" :key="tool" class="badge badge-info mr-2">
+                {{ tool }}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      title: 'Projects'
+      title: 'Projects',
+      projects: [
+        {
+          id: 1,
+          title: 'LVA',
+          description: 'Undergrad Capstone Project Student Information Management System for Lake View Academy in Don Carlos, Bukidnon built in Laravel Framework & Vue.js',
+          github_link: 'https://github.com/kertnik05/LVA',
+          tools: ['PHP', 'HTML', 'CSS', 'JavaScript', 'Laravel', 'Vue.js']
+        },
+        {
+          id: 2,
+          title: 'e-commerce',
+          description: 'Ecommerce app built with Laravel & Vue.js',
+          github_link: 'https://github.com/kertnik05/e-commerce',
+          tools: ['PHP', 'HTML', 'CSS', 'JavaScript', 'Laravel', 'Vue.js']
+        },
+        {
+          id: 3,
+          title: 'vue-boilerplate',
+          description: 'Vue.js 3 Single File Component Biolerplate using Laravel Mix',
+          github_link: 'https://github.com/kertnik05/vue-boilerplate',
+          tools: ['HTML', 'CSS', 'JavaScript', 'Vue.js', 'Laravel Mix']
+        }
+      ]
     }
   },
 
